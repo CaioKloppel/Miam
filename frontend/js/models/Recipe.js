495e7e537e6a47a79a25f04a3b50efa327d7db56
Caio Kloppel <caiokloppelbr@gmail.com>
2025-10-28 10:16:10 -0300

    updating the Recipe class/adding characteristics

--- frontend/js/models/Recipe.js
+++ frontend/js/models/Recipe.js
@@ -1,9 +1,21 @@
 export class Recipe {
-  constructor(idRecipe, name, portions, rating, image, steps, ingredients) {
+  constructor(
+    idRecipe,
+    name,
+    category,
+    portions,
+    rating,
+    favorite,
+    image,
+    steps,
+    ingredients
+  ) {
     this.idRecipe = idRecipe;
     this.name = name;
+    this.category = category;
     this.portions = portions;
     this.rating = rating;
+    this.favorite = favorite;
     this.image = image;
     this.steps = steps;
     this.ingredients = ingredients;
